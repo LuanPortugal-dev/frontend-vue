5bf7345b0d5eb59a1666a75dd4c671414a764a78
Luan Portugal <luanportugal.profissional.tec@gmail.com>
2022-08-26 04:32:05 -0300

    modifying IP from URL

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export async function search_product(value='') {
 
-    const data = await axios.get(`http://18.237.43.245:8000/scraping/${value}/`)
+    const data = await axios.get(`http://35.162.175.220:8000/scraping/${value}/`)
 
     return data
 
